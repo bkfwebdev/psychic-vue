320d81eaa3cef7dc8d910fcdf3bbe12cc208e031
bkfwebdev <bryant.feld@gmail.com>
2021-08-23 11:00:05 -0400

    data, state, events part 1

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,39 @@ export default {
   name: 'App',
   components: {
   GameBoard,   
+  },
+
+  data() {
+    return {
+      snarkyComments : [
+					"!!!... ",
+					" WOW!, your psychic abilities are amazing!.",
+					" You can do better,maybe after a cup of coffee.",
+					" Okay maybe you are not psychic...today",
+					" Perhaps you were distracted...",
+					" Pick this one first next time, trust your instincts.",
+					" It was only a matter of time...right?",
+					" Sorry kid, you aint got it...",
+          " Dude! WTF, you only had 7 choices!",
+          " You need to find a short bridge & take a long walk"
+					],
+        myColors:["red","orange","yellow","green","blue","indigo","violet"],
+        numOfGuesses: 0,
+        computerChoice: null,
+        userChoice: null,   
+    }
+  },
+
+
+  methods:{
+    /* create methods for hadeling clicks and updating game data/game progress */
+
+  },
+
+
+
+  created() {
+    /* generate computer's choice */ 
   }
 }
 </script>
